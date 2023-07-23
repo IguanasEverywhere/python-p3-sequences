836487e411dc293b4b7c16618f6fa325bc95c161
Scott Schwab <scottschwab86@gmail.com>
2023-07-23 15:25:19 -0500

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length == 1:
+        fib_list.append(0)
+    elif length == 2:
+        fib_list.append(0)
+        fib_list.append(1)
+    elif length != 0:
+        fib_list.append(0)
+        fib_list.append(1)
+        for x in range(2, length):
+            fib_list.append(fib_list[x-1] + fib_list[x-2])
+
+    print(fib_list)
+
